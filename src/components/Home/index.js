0e31f5b96be46207c861db16947092bcb7242467
harishreddyshetty <harish.redyshetty@gmail.com>
2023-06-19 17:32:24 +0530

    "commit"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,19 @@
-const Home = () => (
+import { useNavigate } from "react-router-dom";
 
 
+const Home = () => {
+
+    const navigate = useNavigate();
+    
+  const onClickBtn = () => {
+    navigate("/test");
+  };
+
+  return (
     <>
-    <h1>Welcome to home</h1>
+      <h1>Welcome to home</h1>
+      <button type="button" onClick={onClickBtn}>CLick me</button>
     </>
-)
-
-export default Home
+  );
+};
+export default Home;
